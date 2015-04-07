596e8e7d5e9ddefb69d56ebddb4ee7e7e57cd90d
Jakab Kristóf <jaksi07c8@gmail.com>
2015-04-08 01:30:48 +0200

    Block hyperlink auditing

--- examples/policy_http.py
+++ examples/policy_http.py
@@ -12,9 +12,16 @@ class MatrixProxy(HttpProxyNonTransparent):
 		'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36',
 		'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/600.3.18 (KHTML, like Gecko) Version/8.0.3 Safari/600.3.18',
 	]
+
 	def config(self):
 		HttpProxyNonTransparent.config(self)
 		self.request_header["User-Agent"] = (HTTP_HDR_CHANGE_VALUE, random.choice(MatrixProxy.user_agents))
+		self.request["POST"] = (HTTP_REQ_POLICY, self.blockAudits)
+
+	def blockAudits(self, method, url, version):
+		if self.getRequestHeader('Content-Type') == 'text/ping':
+                        return HTTP_REQ_REJECT
+		return HTTP_REQ_ACCEPT
 
 
 def zorp_http():
